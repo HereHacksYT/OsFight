b7c547c8d80125b3c041e9c0c99cab74e37f4256
HereHacksYT <osmanburdacomeback@gmail.com>
2026-08-26 17:05:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('OsFight sunucusu çalışıyor! 🚀');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-app`.listen(port, () => {
-  console.log(`Sunucu ${port} portunda çalışpackageıyor.`);
+app.listen(port, () => {
+  console.log(`OsFight sunucusu ${port} portunda çalışıyor!`);
 });
